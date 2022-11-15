d2668f14a0626353abe400dbcc26a57458be9f8d
Lucas Martins <104236229+W4RT1N5@users.noreply.github.com>
2022-11-15 11:37:34 -0300

    Add files via upload

--- pop-up/js/editar.js
+++ pop-up/js/editar.js
@@ -3,13 +3,13 @@
             '<span></span>' +
             '<span></span>' +
             '<span></span>' +
-            '<!--<a href="https://www.instagram.com/p/B2X-1Z_gGZA/?utm_source=ig_web_copy_link" target="_blank" class="bio_btn">Saiba Mais</a>-->' +
+            '<a href="https://wa.me/5561992975900/?text=Oi,%20gostaria%20de%20saber%20mais%20sobre%20a%20promoção%20do%20kit%20de%20pincéis%20Glam." target="_blank" class="bio_btn">Saiba Mais</a>' +
             '</div>',
             css: '#bio_ep {width: 560px; height: 560px; color: #333; background-color: #fafafa; text-align: center;background-image: url(pop-up/imagens-pop/promocao.png);background-size: cover;}' +
             '#bio_ep_content {padding: 255px 0 0 0; font-family: inherit;}' +
             '#bio_ep_content span:nth-child(2) {display: block; color: #f21b1b; font-size: 32px; font-weight: 600;}' +
             '#bio_ep_content span:nth-child(3) {display: block; font-size: 16px;}' +
             '#bio_ep_content span:nth-child(4) {display: block; margin: -5px 0 0 0; font-size: 16px; font-weight: 600;}' +
-            '.bio_btn {display: inline-block; margin: 18px 0 0 0; padding: 8px 25px 8px 25px; color: #fff; font-size: 19px; font-weight: 900; background-color: #F0A7A0; border: 1px solid #fff; cursor: pointer; -webkit-appearance: none; -moz-appearance: none; border-radius: 100px; text-decoration: none;}',
+            '.bio_btn {display: inline-block; margin: 325px 0 0 0; padding: 8px 25px 8px 25px; color: #fff; font-size: 19px; font-weight: 900; background-color: #F0A7A0; border: 1px solid #fff; cursor: pointer; -webkit-appearance: none; -moz-appearance: none; border-radius: 100px; text-decoration: none;}',
             cookieExp: 0
         });
